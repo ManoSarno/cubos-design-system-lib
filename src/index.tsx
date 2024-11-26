d9bdf83ee2f79c4f82168dd40d562f8e138a0172
ManoSarno <manusarno97@gmail.com>
2024-11-26 11:43:17 -0300

    chore: export input component

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import Chip, { ChipProps } from './components/Chip/Chip';
 import Button, { ButtonProps } from './components/Button/Button';
+import Chip, { ChipProps } from './components/Chip/Chip';
+import Input, { InputProps } from './components/Input/Input';
 
-export { Chip, ChipProps, Button, ButtonProps };
+export { Button, ButtonProps, Chip, ChipProps, Input, InputProps };
