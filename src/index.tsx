f373abd5e59ac7f0dda8248ef6b8085bcb7c21c3
ManoSarno <manusarno97@gmail.com>
2024-11-26 15:56:02 -0300

    chore: export NotificationDialog component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 import Button, { ButtonProps } from './components/Button/Button';
 import Chip, { ChipProps } from './components/Chip/Chip';
 import Input, { InputProps } from './components/Input/Input';
+import NotificationDialog, {
+  NotificationDialogProps,
+} from './components/NotificationDialog/NotificationDialog';
 
-export { Button, ButtonProps, Chip, ChipProps, Input, InputProps };
+export {
+  Button,
+  ButtonProps,
+  Chip,
+  ChipProps,
+  Input,
+  InputProps,
+  NotificationDialog,
+  NotificationDialogProps,
+};
