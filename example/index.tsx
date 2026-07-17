96106e92faac74bfd42fc5d8d42845eaffddae4b
Manoela Sarno Fonseca <manoela.sarno@gmail.com>
2026-07-17 14:14:52 -0300

    feat(example): add component usage demo

--- example/index.tsx
+++ example/index.tsx
@@ -1,10 +1,97 @@
+import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Chip, Button, Input, NotificationDialog, ConfirmDialog, Modal } from '../src';
+import '../src/styles/global.css';
 
 const App = () => {
+  const [open, setOpen] = React.useState(false);
+  const [openConfirm, setOpenConfirm] = React.useState(false);
+  const [openModal, setOpenModal] = React.useState(false);
+
   return (
-    <div style={{ padding: '2rem', fontFamily: 'system-ui, sans-serif' }}>
-      <h1>Component Demo</h1>
+    <div style={{ padding: '32px', position: 'relative', display: 'flex', flexDirection: 'column', gap: '16px' }}>
+      <Chip
+        title="Meu componente Chip"
+        width={300}
+        color="orange"
+        type="outlined"
+      />
+      
+      <Button
+        variant="normal"
+        colors="orange__outline"
+        width={400}
+        onClick={() => setOpen(true)}
+      >
+        Abrir Notification Dialog
+      </Button>
+      
+      <Button
+        variant="normal"
+        colors="orange"
+        width={400}
+        onClick={() => setOpenConfirm(true)}
+      >
+        Abrir Confirm Dialog
+      </Button>
+      
+      <Button
+        variant="normal"
+        colors="white"
+        width={400}
+        onClick={() => setOpenModal(true)}
+      >
+        Abrir Modal
+      </Button>
+      
+      <Input
+        labelId="input-name"
+        labelText="Name"
+        width={300}
+        height={80}
+        icon="https://cdn3.iconfinder.com/data/icons/feather-5/24/settings-512.png"
+        iconPosition="left"
+        placeholder="Digite seu nome"
+      />
+      
+      <NotificationDialog
+        open={open}
+        type="success"
+        title="Meu título!"
+        description="minha descrição do dialog é essa daqui."
+        actionText="Fechar"
+        handleClose={() => setOpen(false)}
+      />
+
+      <ConfirmDialog
+        open={openConfirm}
+        type="error"
+        title="Meu título!"
+        description="minha descrição para o confirm dialog."
+        btnConfirmText="Confirmar"
+        btnCancelText="Cancelar"
+        handleConfirm={() => setOpenConfirm(false)}
+        handleCancel={() => setOpenConfirm(false)}
+      />
+
+      <Modal
+        open={openModal}
+        width={450}
+        height={600}
+        btnConfirmText="Confirmar"
+        btnCancelText="Cancelar"
+        handleConfirm={() => setOpenModal(false)}
+        handleCancel={() => setOpenModal(false)}
+      >
+        <h1>Meu Modal</h1>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '32px' }}>
+          <h2>Meu sub título</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed cumque nulla exercitationem rerum eius in optio laborum, aut excepturi temporibus quam perferendis! Omnis tenetur incidunt aliquam voluptates vel delectus harum!
+          </p>
+        </div>
+      </Modal>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,9 @@
-import './styles/global.css';
+import Chip, { ChipProps } from './components/Chip/Chip';
+import Button, { ButtonProps } from './components/Button/Button';
+import Input, { InputProps } from './components/Input/Input';
+import NotificationDialog, { NotificationDialogProps } from './components/NotificationDialog/NotificationDialog';
+import ConfirmDialog, { ConfirmDialogProps } from './components/ConfirmDialog/ConfirmDialog';
+import Modal, { ModalProps } from './components/Modal/Modal';
 
-export { default as Button } from './components/Button/Button';
-export type { ButtonProps } from './components/Button/Button';
-
-export { default as Chip } from './components/Chip/Chip';
-export type { ChipProps } from './components/Chip/Chip';
-
-export { default as Input } from './components/Input/Input';
-export type { InputProps } from './components/Input/Input';
-
-export { default as Modal } from './components/Modal/Modal';
-export type { ModalProps } from './components/Modal/Modal';
-
-export { default as ConfirmDialog } from './components/ConfirmDialog/ConfirmDialog';
-export type { ConfirmDialogProps } from './components/ConfirmDialog/ConfirmDialog';
-
-export { default as NotificationDialog } from './components/NotificationDialog/NotificationDialog';
-export type { NotificationDialogProps } from './components/NotificationDialog/NotificationDialog';
-
-export { default as ArrowBackIcon } from './components/Icons/ArrowBackIcon';
-export { default as CautionIcon } from './components/Icons/CautionIcon';
-export { default as SuccessIcon } from './components/Icons/SuccessIcon';
+export { Chip, Button, Input, NotificationDialog, ConfirmDialog, Modal };
+export type { ChipProps, ButtonProps, InputProps, NotificationDialogProps, ConfirmDialogProps, ModalProps };
